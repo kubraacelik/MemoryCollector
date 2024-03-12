6b91671a0f7f59d720e69af64880d6a2724a72f1
kubraacelik <kkbra.celik92@gmail.com>
2024-03-12 16:15:28 +0300

    added GoChevronDown and GoChevronLeft icons

---
+++ memorycollector/src/components/ExpandablePanels.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { GoChevronDown } from "react-icons/go";
+import { GoChevronLeft } from "react-icons/go";
+import { useState } from "react";
+
+function ExpandablePanels({ children, header }) {
+  const [expanded, setExpanded] = useState(false);
+
+  // açıksa kapansın, kapalıysa açılsın
+  const handleClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <div className="panelDiv">
+      <div className="topArrangement">
+        <div className="topArrangement">{header}</div>
+        <div onClick={handleClick} value={expanded}>
+          {expanded ? (
+            <GoChevronDown style={{ cursor: "pointer" }} />
+          ) : (
+            <GoChevronLeft style={{ cursor: "pointer" }} />
+          )}
+        </div>
+      </div>
+
+      {/* expanded && : expanded doğru olduğu müddetçe demek */}
+      {expanded && <div>{children}</div>}
+    </div>
+  );
+}
+
+export default ExpandablePanels;
