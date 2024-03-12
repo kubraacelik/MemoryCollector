91f0d8eedaae2d0c28a3b921b9a3867361e8808e
kubraacelik <kkbra.celik92@gmail.com>
2024-03-12 15:02:42 +0300

    Added fetchFn to discard all requests in 1sec intervals

--- memorycollector/src/store/apis/usersApi.js
+++ memorycollector/src/store/apis/usersApi.js
@@ -1,18 +1,32 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { faker } from "@faker-js/faker";
+
+// her isteğin belirli bir süre sürmesi için
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 // reducerPath:index.js'ten ona hangi isimle ulaşaçağımı belirtir
 // baseQuery:nereye istek atacağımı belirtir
 // endpoints:data çekme, ekleme, kişileri kaldırma gibi işlemleri burada yaparız
+// fetchFn:1sn arayla tüm istekleri atacak
 const usersApi = createApi({
   reducerPath: "users",
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:3000",
+    fetchFn: async (...args) => {
+      await pause(1000);
+      return fetch(...args);
+    },
   }),
   endpoints(builder) {
     return {
       // fetchUsers: kişileri çekme işlemini yapacağız
       //  builder.query: data'yı çekmek için kullanılır
       fetchUsers: builder.query({
+        providesTags: ["User"],
         query: () => {
           return {
             // url : nereye istek atacağımı gösterir(db.json'daki ismini aldık)
@@ -25,6 +39,9 @@ const usersApi = createApi({
       // addUser: kişi ekleme işlemini yapacağız
       //  builder.mutation: data'yı update etmek için kullanılır
       addUser: builder.mutation({
+        invalidatesTags: () => {
+          return [{ type: "User" }];
+        },
         query: () => {
           return {
             // veri ekleyeceğim için POST metodunu kullandık
@@ -32,7 +49,7 @@ const usersApi = createApi({
             url: "/users",
             method: "POST",
             body: {
-              name: "Selena",
+              name: faker.name.fullName(),
             },
           };
         },
@@ -40,6 +57,9 @@ const usersApi = createApi({
       // removeUser: kişi silme işlemini yapacağız
       //   silme işlemi yapacağım için parametre olarak bir user bilgisi almam lazım
       removeUser: builder.mutation({
+        invalidatesTags: () => {
+          return [{ type: "User" }];
+        },
         query: (user) => {
           return {
             // veri sileceğim için DELETE metodunu kullandık(silme işleminde users/id olarak yazılır)
