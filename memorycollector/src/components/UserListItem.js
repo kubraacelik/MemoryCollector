ec3cac0cda3bcfe584b29fe4f6bd49bc011f3aba
kubraacelik <kkbra.celik92@gmail.com>
2024-03-12 16:17:26 +0300

    Delete button activated for deletion

--- memorycollector/src/components/UserListItem.js
+++ memorycollector/src/components/UserListItem.js
@@ -1,7 +1,38 @@
 import React from "react";
+import ExpandablePanels from "./ExpandablePanels";
+import AlbumList from "./AlbumList";
+import { GoTrash } from "react-icons/go";
+import { useRemoveUserMutation } from "../store/apis/usersApi";
+import { CircularProgress } from "@mui/material";
 
 export default function UserListItem({ user }) {
+  const [removeUser, results] = useRemoveUserMutation();
+
+  const handleClick = () => {
+    removeUser(user);
+  };
+
+  const header = (
+    <>
+      <button
+        style={{ marginRight: "30px", border: "none", cursor: "pointer" }}
+        onClick={handleClick}
+      >
+        {results.isLoading ? (
+          <CircularProgress style={{ width: "20px", height: "20px" }} />
+        ) : (
+          <GoTrash />
+        )}
+      </button>
+      {user.name}
+    </>
+  );
+
   return (
-    <div>{user.name}</div>
+    <div>
+      <ExpandablePanels header={header}>
+        <AlbumList user={user} />
+      </ExpandablePanels>
+    </div>
   );
 }
