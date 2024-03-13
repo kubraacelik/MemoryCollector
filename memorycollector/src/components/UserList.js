834ae4a6489c28ea7609ba1e8e59611454ff3685
kubraacelik <kkbra.celik92@gmail.com>
2024-03-13 17:13:57 +0300

    added descriptions

--- memorycollector/src/components/UserList.js
+++ memorycollector/src/components/UserList.js
@@ -3,6 +3,8 @@ import { useFetchUsersQuery, useAddUserMutation } from "../store";
 import UserListItem from "./UserListItem";
 import { Skeleton, Button, CircularProgress } from "@mui/material";
 
+//? KİŞİLERİ LİSTELEMEK İÇİN
+
 export default function UserList() {
   // veri çekme işlemi yapıyoruz
   const { data, isError, isFetching } = useFetchUsersQuery();
@@ -17,13 +19,17 @@ export default function UserList() {
   // content benim sayfamdaki görüntü
   let content;
 
+  //ilk defa yükleniyorsa
   if (isFetching) {
     content = (
       <Skeleton variant="rectangular" sx={{ width: "100%", height: "600px" }} />
     );
+    //hata varsa
   } else if (isError) {
-    content = <div>Hata Var</div>;
-  } else {
+    content = <div style={{fontSize:'20px', color:'red'}}>Hata Var</div>;
+  } 
+  //hata yok ve data geliyorsa
+  else {
     content = data.map((user) => {
       return <UserListItem key={user.id} user={user} />;
     });
@@ -32,7 +38,7 @@ export default function UserList() {
   return (
     <div>
       <div className="topArrangement">
-        <h1 style={{ fontSize: "25px" }}>Kişiler</h1>
+        <h1 style={{ fontSize: "40px" }}>Kişiler</h1>
         <Button variant="outlined" onClick={handleUserAdd}>
           {results.isLoading ? <CircularProgress /> : <span>Kişi Ekle+</span>}
         </Button>
